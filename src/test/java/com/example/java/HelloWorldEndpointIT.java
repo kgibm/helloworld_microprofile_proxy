a64af9dbb0418ccf6b8b3be22e3566603ce6077a
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2021-05-10 15:59:18 -0700

    Optimize test structure

--- src/test/java/com/example/java/HelloWorldEndpointIT.java
+++ src/test/java/com/example/java/HelloWorldEndpointIT.java
@@ -8,9 +8,18 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldEndpointIT {
+	private static Client client;
+
+	@BeforeAll
+	public static void setup() {
+		client = ClientBuilder.newClient();
+	}
+
 	@Test
 	public void test1() {
 		WebTarget target = getTarget("/api/helloworldjaxrs");
@@ -21,8 +30,11 @@ public class HelloWorldEndpointIT {
 	}
 
 	private WebTarget getTarget(String path) {
-		Client client = ClientBuilder.newBuilder().build();
-		WebTarget target = client.target("http://localhost:" + System.getProperty("http.port") + path);
-		return target;
+		return client.target("http://localhost:" + System.getProperty("http.port") + path);
+	}
+
+	@AfterAll
+	public static void stop() {
+		client.close();
 	}
 }
