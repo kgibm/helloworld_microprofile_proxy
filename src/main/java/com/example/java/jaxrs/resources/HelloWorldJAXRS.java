50fa645c3c286bd7ab435c565ebbc5b66d41469b
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2021-05-11 08:41:08 -0700

    Add sleepTimeMs option to test requestTiming

--- src/main/java/com/example/java/jaxrs/resources/HelloWorldJAXRS.java
+++ src/main/java/com/example/java/jaxrs/resources/HelloWorldJAXRS.java
@@ -6,9 +6,11 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("")
@@ -21,12 +23,18 @@ public class HelloWorldJAXRS {
 	@Path("helloworldjaxrs")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Map<String, Object> helloWorldJAXRS() {
+	public Map<String, Object> helloWorldJAXRS(@DefaultValue("0") @QueryParam("sleepTimeMs") long sleepTimeMs)
+			throws Throwable {
+
 		if (LOG.isLoggable(Level.FINER))
 			LOG.entering(CLASS_NAME, METHOD_HELLOWORLDJAXRS);
 
 		Map<String, Object> json = new HashMap<>();
 		json.put("message", "Hello World @ " + Instant.now());
+
+		if (sleepTimeMs > 0) {
+			Thread.sleep(sleepTimeMs);
+		}
 
 		if (LOG.isLoggable(Level.FINER))
 			LOG.exiting(CLASS_NAME, METHOD_HELLOWORLDJAXRS, json);
